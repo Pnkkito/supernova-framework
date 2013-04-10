46105818e2b7994eaea3b675137c335a6097817b
Jose Luis Gonzalez <jl.gonzalezmedel@gmail.com>
2013-04-10 20:34:36 -0300

    [View] Sidebar element

---
+++ application/views/elements/sidebar.php
@@ -0,0 +1,4 @@
+<li><?=$this->html->link('<i class="icon-align-justify"></i> Instalation',array('controller' => 'pages', 'action' => 'install'));?></li>
+
+<!-- Posteo -->
+<li><?=$this->html->link("<i class='icon-align-justify'></i> Posteo",array("controller" => "posteos", "action" => "index"));?></li>
