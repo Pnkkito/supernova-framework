78bd11fe283d0c292bf9ba7f9d8dc04c0a4c135b
Jose Luis Gonzalez <jl.gonzalezmedel@gmail.com>
2013-04-10 20:42:24 -0300

    [Install] Minor fix view

--- application/views/pages/install.php
+++ application/views/pages/install.php
@@ -3,6 +3,7 @@
 	<p>Select the blackhole MVC you wish to create automagicaly in your application<p>
 	<?=$this->html->form('create');?>
 	<?=$this->html->select('tablename','Model name',array('options' => $tables));?>
+	<div style="clear:both;"></div>
 	<?=$this->html->form('submit','Create this blackhole');?>
 	<?=$this->html->form('end');?>
 </div>
