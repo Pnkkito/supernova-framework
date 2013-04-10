c9440fda8a1aafae2b74adc4e917a334059c0de6
Jose Luis Gonzalez <jl.gonzalezmedel@gmail.com>
2013-04-10 20:43:54 -0300

    [Install] Mayor changes
    Modified the entire function,
    replaced to work in MVC style

--- application/controllers/pages_controller.php
+++ application/controllers/pages_controller.php
@@ -1,151 +1,57 @@
 <?php
 class pagesController extends AppController {
 	
-	// function passwordtest(){
+	// function authExample(){
 	// 	$this->Auth = new Auth();
 	// 	$hash = $this->Auth->passwordHash('hola mundo');
 	// 	$verify = $this->Auth->passwordVerify('holamundo','hola mundo');
 	// }
 
-	function index(){
-		
-	}
-
 	function install(){
 		$this->layout("management");
-		$tables = SQLQuery::getTables();
-		// sort($tables);
-		
+		$conection = new SQLQuery;
+		$tables = $conection->getTables();
+
 		if ($this->data){
+			//Setting vars
 			$table = $this->data['Page']['tablename'];
 			$prefix = substr($table,0,2);
 			$index = substr($table,2,2);
 			$defaultPK = "ID";
 			$defaultDisplayField = "ID";
 			$modelName = $tables[$table];
-			// $modelName = Inflector::singularize(Inflector::under_to_camel(strtolower(substr($table,5))));
+			$fields = $conection->getFields($table);
+			$fieldsUnparsed = Inflector::unparseFields($fields,$modelName);
+			$this->set(compact('modelName','index','defaultPK','defaultDisplayField','table','prefix','fields','fieldsUnparsed'));
 			
-			//Building Model File
-			$outputModel = '<?php
-
-class '.$modelName.' extends Model {
-
-	var $index = "'.$index.'";
-	var $primaryKey = "'.$defaultPK.'";
-	var $displayField = "'.$defaultDisplayField.'";
-
-}
-';
-
-			$fileName = strtolower($modelName).".php";
+			//Build Model file
 			$dirName = ROOT.'/application/models/';
-			chdir(ROOT);
-			chdir('./application/models');
-
-			file_put_contents($fileName,$outputModel);
-
-
-
-			//Building Controller File
-			$outputController = '<?php
-class '.strtolower(Inflector::pluralize($modelName)).'Controller extends AppController {
-
-	function index(){
-		$'.strtolower(Inflector::pluralize($modelName)).' = $this->'.$modelName.'->find("all");
-		$this->set(compact("'.strtolower(Inflector::pluralize($modelName)).'"));
-	}
-
-	function add(){
-		if ($this->data){
-			$this->'.$modelName.'->save($this->data);
-			$this->setMessage("New data saved");
-			$this->redirect(array("controller" => "'.strtolower(Inflector::pluralize($modelName)).'", "action" => "index"));	
-		}
-	}
-
-	function edit($id = null){
-		if ($id){
-			$'.strtolower($modelName).' = $this->'.$modelName.'->findBy("ID",$id);
-			$this->set(compact("'.strtolower($modelName).'"));
-		}
-		if ($this->data){
-			$this->'.$modelName.'->save($this->data);
-			$this->setMessage("Data saved");
-			$this->redirect(array("controller" => "'.strtolower(Inflector::pluralize($modelName)).'", "action" => "index"));
-		}
-	}
-
-	function delete($id = null){
-		if ($id){
-			$this->'.$modelName.'->delete($id);
-			$this->setMessage("Data deleted");
-			$this->redirect(array("controller" => "'.strtolower(Inflector::pluralize($modelName)).'", "action" => "index"));		
-		}
-	}
-}
-';
-
+			$fileName = strtolower($modelName).".php";
+			$this->render("templates/outputModel", "file", $dirName.$fileName);
+			
+			//Build Controller file
 			$fileName = Inflector::pluralize(Inflector::camel_to_under($modelName))."_controller.php";
 			$dirName = ROOT.'/application/controllers/';
-			chdir(ROOT);
-			chdir('./application/controllers');
-			file_put_contents($fileName,$outputController);
-
-			//Building Views for Model
-			$fields = SQLQuery::getFields($table);
-			$fieldsUnparsed = Inflector::unparseFields($fields,$modelName);
+			$this->render("templates/outputController", "file", $dirName.$fileName);
 			
-			$indexOutputFile = '<h1>'.$modelName.' List</h1>
-<?php
-echo $this->html->table($'.strtolower(Inflector::pluralize($modelName)).', array("actions" => true));
-echo "<div class=\'buttons\'>";
-echo $this->html->link("Add '.strtolower($modelName).'",array("controller" => "'.strtolower(Inflector::pluralize($modelName)).'","action" => "add"));
-echo "</div>";';
-
-
-			$editOutputFile = '<h1>Edit '.Inflector::singularize($modelName).'</h1>
-<?php
-echo $this->html->form("create");';
-			foreach ($fieldsUnparsed as $eachfield => $typefield){
-				if (!empty($eachfield)){
-					if ($eachfield == $defaultPK){
-						$editOutputFile.='echo $this->html->input("'.$defaultPK.'","",array("type" => "hidden","value" => $'.strtolower($modelName).'["'.$modelName.'"]["'.$defaultPK.'"]));';
-					}else{
-						$editOutputFile.='echo $this->html->input("'.$eachfield.'","'.Inflector::under_to_camel(strtolower($eachfield)).'", array("value" => $'.strtolower($modelName).'["'.$modelName.'"]["'.$eachfield.'"]));';
-					}
-				}
-			}
-$editOutputFile.='$this->html->form("submit","Save changes");
-echo $this->html->form("end");';
-
-
-			$addOutputFile = '<h1>Add '.Inflector::singularize($modelName).'</h1>
-<?php
-echo $this->html->form("create");';
-			foreach ($fieldsUnparsed as $eachfield => $typefield){
-				if (!empty($eachfield)){
-					if ($eachfield != $defaultPK){
-						$addOutputFile.='echo $this->html->input("'.$eachfield.'","'.Inflector::under_to_camel(strtolower($eachfield)).'");';
-					}
-				}
-			}
-
-$addOutputFile.='echo $this->html->form("submit","Add new '.Inflector::singularize($modelName).'");
-echo $this->html->form("end");';
-
+			//Build Views files
 			$dirName = ROOT.'/application/views/'.Inflector::pluralize(Inflector::camel_to_under($modelName));
 			chdir(ROOT);
 			chdir('./application/views');
-			mkdir(Inflector::pluralize(Inflector::camel_to_under($modelName)), 0777, true);
-			chdir('./'.Inflector::pluralize(Inflector::camel_to_under($modelName)));
-			file_put_contents('index.php',$indexOutputFile);
-			file_put_contents('edit.php',$editOutputFile);
-			file_put_contents('add.php',$addOutputFile);
+			$modelDirName = Inflector::pluralize(Inflector::camel_to_under($modelName));
+			if (!file_exists($modelDirName)){
+				mkdir($modelDirName, 0777, true);
+			}
+			$this->render("templates/indexOutputFile", "file", $dirName."/index.php");
+			$this->render("templates/editOutputFile", "file", $dirName."/edit.php");
+			$this->render("templates/addOutputFile", "file", $dirName."/add.php");
+			$newLink = "\n"."<!-- ".$modelName." -->"."\n";
+			$newLink.= '<li><?=$this->html->link("<i class=\'icon-align-justify\'></i> '.$modelName.'",array("controller" => "'.Inflector::pluralize(Inflector::camel_to_under($modelName)).'", "action" => "index"));?></li>'."\n";
+			file_put_contents(ROOT.'/application/views/elements/sidebar.php',$newLink,FILE_APPEND);
 
-			$this->setMessage('All BlackHole files created successfully');
+			$this->setMessage('All BlackHole files created successfully','success');
 		}
 		$this->set(compact('tables'));
 	}
 	
 }
-?>
