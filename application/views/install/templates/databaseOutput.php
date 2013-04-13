a89d7c3aa8a117c25f80528753d561fa8db41b8f
Jose Luis Gonzalez <jl.gonzalezmedel@gmail.com>
2013-04-12 21:57:18 -0300

    [Template] Generate database.ini file

---
+++ application/views/install/templates/databaseOutput.php
@@ -0,0 +1,8 @@
+[Database]
+DB_NAME = "<?php echo $this->data['Install']['dbname'];?>"
+DB_USER = "<?php echo $this->data['Install']['user'];?>"
+DB_PASS = "<?php echo $this->data['Install']['pass'];?>"
+DB_HOST = "<?php echo $this->data['Install']['host'];?>"
+DB_DRIVER = "<?php echo $this->data['Install']['driver'];?>"
+DBPREFIX = "<?php echo $this->data['Install']['prefix'];?>"
+DBS = "_"
