1c1bb023ce72c9f0865bb622613a1ebf36d9f49a
Jose Luis Gonzalez <jl.gonzalezmedel@gmail.com>
2013-06-12 04:01:47 -0300

    blackhole2 view

---
+++ application/views/install/blackhole2.php
@@ -0,0 +1,24 @@
+<h3>Welcome to Supernova Blackhole</h3>
+<p><strong>Remember:</strong></p>
+<br/>
+<table style="width: 100%">
+	<td>
+		<ul>
+			<li><strong>config.json</strong> - Base config</li>
+			<li><strong>database.json</strong> - Database config</li>
+			<li><strong>schema.json</strong> - Database schema for your application</li>
+		<ul>
+	</td>
+	<td>
+		<ul>
+			<li><strong>fixture.json</strong> - Database data automatic filling</li>
+			<li><strong>packagename_plugin.zip</strong> - Plugins for your application</li>
+			<li><strong>layoutname_layout.zip</strong> - Layout package for your application</li>
+		</ul>
+	</td>
+</table>
+<br/>
+<div class="blackhole">
+<p>Drop your file into the blackhole,
+and this will be transformed and adapted for your application</p>
+</div>
